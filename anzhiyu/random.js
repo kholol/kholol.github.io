dc71b31cf70cd20619a55243555e11ce0f426072
kholol <9639411+fkaohe@user.noreply.gitee.com>
2024-01-01 13:05:07 +0800

    Site updated: 2024-01-01 13:05:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/01/第一篇文章/","2024/01/01/hello-world/"];function toRandomPost(){
+var posts=["2024/01/01/hello-world/","2024/01/01/第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
