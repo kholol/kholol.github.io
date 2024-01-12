8322c6748056cd787aaa8d5e47485f42b509c5d2
kholol <1917299862@qq.com>
2024-01-12 23:49:24 +0800

    Site updated: 2024-01-12 23:49:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/12/分类文章测试/","2024/01/04/测试文章/","2024/01/12/图片插入测试/"];function toRandomPost(){
+var posts=["2024/01/12/分类文章测试/","2024/01/12/图片插入测试/","2024/01/04/测试文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
